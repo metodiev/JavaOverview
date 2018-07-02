b86fb909f0c4392ab2ec8c973ab01e20e223006d
metodiev <miroslav.l.metodiev@gmail.com>
2018-07-02 23:01:14 +0300

    Spring with Mysql connection
    Spring with Mysql connection project example with JSON and MD5 crypting

---
+++ PsringMysqlDatabase/src/main/java/com/database/DatabaseSprinmgApplication.java
@@ -0,0 +1,12 @@
+package com.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseSprinmgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseSprinmgApplication.class, args);
+	}
+}
